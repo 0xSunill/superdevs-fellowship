5b4ad374764572fc1b47b893957a95c85ba0ed78
0xSunill <sunday7637@gmail.com>
2025-07-22 18:35:03 +0000

    fix

--- week-1/custom-macro/target/debug/custom-macro.d
+++
@@ -1,1 +0,0 @@
-/home/sivasunil/blockchain/solana/superdevs-fellowship/week-1/custom-macro/target/debug/custom-macro: /home/sivasunil/blockchain/solana/superdevs-fellowship/week-1/custom-macro/src/main.rs
---
+++ week-1/mannual-dese-se/target/debug/custom-macro.d
@@ -0,0 +1,1 @@
+/home/sivasunil/blockchain/solana/superdevs-fellowship/week-1/mannual-dese-se/target/debug/custom-macro: /home/sivasunil/blockchain/solana/superdevs-fellowship/week-1/mannual-dese-se/src/main.rs
