d99d6a207beac6c48409b36309c24c5ffff4e2a2
0xSunill <sunday7637@gmail.com>
2025-08-07 06:15:22 +0000

    declarative macros

--- week-1/traits/src/main.rs
+++ week-1/traits/src/main.rs
@@ -1,23 +1,58 @@
+// struct Rect {
+//     width: u32,
+//     height: u32,
+// }
+// struct Square {
+//     side: u32,
+// }
+
+// trait Shape {
+//     fn area(&self) -> u32;
+// }
+
+// impl Shape for Rect {
+//     fn area(&self) -> u32 {
+//         return self.height * self.height;
+//     }
+// }
+
+// impl Shape for Square {
+//     fn area(&self) -> u32 {
+//         return self.side * self.side;
+//     }
+// }
+
+// #[derive(Debug)]
+// struct S;
+// fn main() {
+//     let r = Rect {
+//         width: 10,
+//         height: 20,
+//     };
+//     let s = Square { side: 23 };
+//     println!("the area of the Rectangle is {}", r.area());
+//     println!("the area of the Squre is {}", s.area());
+//     println!("the area of the Squre is {}", s.area());
+//     println!("{:?}", S);
+// }
+
 struct Rect {
-    width: u32,
-    height: u32,
-}
-struct Square {
-    side: u32,
+    height: u64,
+    width: u64,
 }
 
-trait Shape {
-    fn area(&self) -> u32;
+struct Squre {
+    side: u64,
 }
 
-impl Shape for Rect {
-    fn area(&self) -> u32 {
-        return self.height * self.height;
+impl Rect {
+    fn area(&self) -> u64 {
+        return self.height * self.width;
     }
 }
 
-impl Shape for Square {
-    fn area(&self) -> u32 {
+impl Squre {
+    fn area(&self) -> u64 {
         return self.side * self.side;
     }
 }
@@ -25,10 +60,11 @@ impl Shape for Square {
 fn main() {
     let r = Rect {
         width: 10,
-        height: 20,
+        height: 40,
     };
-    let s = Square { side: 23 };
-    println!("the area of the Rectangle is {}", r.area());
-    println!("the area of the Squre is {}", s.area());
-}
 
+    let s = Squre { side: 39 };
+
+    println!("the value of  reactangle {}", r.area());
+    println!("the value of  squre {}", s.area());
+}
